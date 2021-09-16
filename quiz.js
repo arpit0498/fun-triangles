7cb96e1757424982183f33006a435d89fd5040ea
arpit gupta <mycontact.arpit@gmail.com>
2021-09-17 03:51:47 +0530

    feat:quiz file completed

--- quiz.js
+++ quiz.js
@@ -1,6 +1,23 @@
-const quizForm = document.querySelector('.question-container');
+const quizForm = document.querySelector('.quiz-form');
 const submitAnswerBtn = document.querySelector('#submit-answer-button');
 
 const output = document.querySelector('#output');
 
-const correct Answer=["90","right angled"];
+const correctAnswer=["90","right angled"];
+
+function calculateScore(){
+    let score = 0;
+    let index=0;
+    const formResults =new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value=== correctAnswer[index]){
+            score++;
+        }
+        index++;
+    }
+
+
+output.innerText ="your score is  " + score;
+}
+
+submitAnswerBtn.addEventListener('click', calculateScore);
