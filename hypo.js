b431a13bce8ddef1c08d7ffe2f003777753db73c
arpit gupta <mycontact.arpit@gmail.com>
2021-09-17 04:28:16 +0530

    feat:hypotenuse logic

---
+++ hypo.js
@@ -0,0 +1,19 @@
+const sides = document.querySelectorAll('.side-input');
+const hypotenuseBtn=document.querySelector('#hypotenuse-btn');
+const output =document.querySelector('#output');
+
+function calculateSquareSum(a,b){
+    const sumSquare=a*a + b*b ;
+    // console.log(sumSquare);
+    return sumSquare;
+}
+
+function calculateHypotenuse(){
+    const sumSquare = calculateSquareSum(Number(sides[0].value),Number (sides[1].value));
+    const lengthHypotenuse= Math.sqrt(sumSquare);
+    output.innerText="Length of hypotenuse: "+ lengthHypotenuse
+    
+
+}
+
+hypotenuseBtn.addEventListener("click", calculateHypotenuse);
