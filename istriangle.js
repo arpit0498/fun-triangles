140955964256dd12eb25d68b2502336fec0e7dac
arpit gupta <mycontact.arpit@gmail.com>
2021-09-16 17:33:42 +0530

    feat: sum of angle

--- istriangle.js
+++ istriangle.js
@@ -3,4 +3,23 @@ const isTriangleBtn=document.querySelector('#is-triangle-btn');
 const outputE1 =document.querySelector('#output');
 
 
-console.log(inputs[0]);
+
+function calculateSumOfAngles(angle1,angle2,angle3)
+{
+    const sumOfAngles= angle1 + angle2 + angle3;
+    return sumOfAngles;
+}
+
+function isTriangle()
+{
+    const sumOfAngles = calculateSumOfAngles
+    (Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
+    if(sumOfAngles === 180){
+        console.log("forms a triangle")
+    }
+    else{
+         console.log("Doesn't form triangle" );
+    }  
+
+}
+isTriangleBtn.addEventListener("click", isTriangle)
