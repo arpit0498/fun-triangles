457f944a01055a059a442a659a898195c98f2e61
arpit gupta <mycontact.arpit@gmail.com>
2021-09-17 03:24:02 +0530

    quiz : ds added to quiz js

---
+++ quiz.js
@@ -0,0 +1,6 @@
+const quizForm = document.querySelector('.question-container');
+const submitAnswerBtn = document.querySelector('#submit-answer-button');
+
+const output = document.querySelector('#output');
+
+const correct Answer=["90","right angled"];
