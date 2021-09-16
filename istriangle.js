9ce3115725de0a0441c1299816167b435219ebe8
arpit gupta <mycontact.arpit@gmail.com>
2021-09-16 16:58:10 +0530

    feat:js added query selectors

---
+++ istriangle.js
@@ -0,0 +1,6 @@
+const inputs =document.querySelectorAll('.input-angle');
+const isTriangleBtn=document.querySelector('#is-triangle-btn');
+const outputE1 =document.querySelector('#output');
+
+
+console.log(inputs[0]);
